ae2e93f537d8043db15b2eb71c814b6281b47ef5
BBAK <shwysh123@naver.com>
2023-05-23 15:07:22 +0900

    1day

---
+++ src/Ch01/C01HelloWorld.java
@@ -0,0 +1,15 @@
+package Ch01; // 패키지명
+
+public class C01HelloWorld // 클래스영역 - 객체지향 문법 적용 단위
+{
+	// 메서드 - 작업수행을 하는데 필요한 여러 코드를 묶어서 실행하는 단위
+	// main 메서드 - 최초 실행 함수
+	// library 메서드 -  미리 만들어져 제공되어지는 메서드
+	// 사용자 정의 메서드 - 개발자의 의해 만들어지는 메서드
+	public static void main(String[] args) //메서드 영역 - 절차 지향 문법 적용 단위
+{
+		// TODO Auto-generated method stub
+		System.out.println("What the Fuck"); // 라이브러리 메서드 호출
+	}
+
+}
---
+++ src/Ch01/C02SystemOut.java
@@ -0,0 +1,32 @@
+package Ch01;
+
+public class C02SystemOut {
+
+	public static void main(String[] args) {
+//		 TODO Auto-generated method stub
+		//단축키
+		//복사 : 컨트롤 + 알트 + 아래
+		//주석 : 컬트롤 + /
+		//System : OS
+		//System.out : OS가 관리하는 표준 출력 스트림
+		//System.out.print();
+		//ESCAPE 문자 :
+		// \n : 개행, 줄바꿈 \b : 백스페이스 \t : 탭길이(기본 8칸) 만큼 커서이동
+		System.out.print(30);
+		System.out.print("30");
+		System.out.print("E");
+		System.out.print("I'm Gooo\ting");
+		
+//		System.out.printf();
+		//format : 형식,서식
+		//%d = 10진수 정수 서식문자 double
+		//%f = 10진수 실수 서식문자 float
+		//%c = 한문자 서식문자 char
+		//%s = 문자열 서식문자 String
+		System.out.printf("%d %d %d \n", 10,20,30);
+		System.out.printf("%f %f %f \n", 10.2,20.3,30.1);
+		System.out.printf("%c %c %c \n", 'a','b','c');
+		System.out.printf("%d %s %d \n", 1,"국어",100);
+	}
+
+}
